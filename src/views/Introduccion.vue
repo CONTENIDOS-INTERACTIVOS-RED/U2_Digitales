a9591e929c796b793bed046efaf8bcebdd12bf34
Francisco Lizcano <flizcanor@icloud.com>
2025-07-12 20:17:37 -0500

    Update introduction layout and content; replace images and enhance descriptions for educational resources.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -3,63 +3,39 @@
   BannerInterno(subTitulo="Introducción")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
 
-    .bg-full-width.bg-color-5
-      .p-4.p-md-5
-        .row.mb-5
-          .col-lg-4.order-lg-2.mb-3.mb-lg-0
-            figure
-              img(src="@/assets/curso/intro/1.png", data-aos="zoom-in")
-          .col-lg-8.order-lg-1
-            p(data-aos="fade-left") El panorama educativo contemporáneo, se caracteriza por una creciente adopción y relevancia de los entornos virtuales de aprendizaje. Estas plataformas digitales han transformado la manera en que se imparte y se accede a la educación, ofreciendo flexibilidad y oportunidades de aprendizaje, a una escala sin precedentes. En este contexto, la calidad y pertinencia de los Recursos Educativos Digitales (RED), se han convertido en un factor crítico para el éxito de la educación virtual. 
-        
-            .bg-color-1.p-4.mb-3(data-aos="fade-left")
-              p.mb-0 Los RED, cuando están bien diseñados, tienen el potencial de mejorar significativamente la participación de los estudiantes, optimizar los resultados del aprendizaje y garantizar una mayor accesibilidad a la educación en línea. 
-            
-            p Esta unidad se centrará en los fundamentos de la producción de RED efectivos, proporcionando a los estudiantes de la Especialización en Innovación Educativa en Entornos Virtuales de Aprendizaje, las herramientas conceptuales y prácticas necesarias para diseñar y desarrollar materiales didácticos digitales de alta calidad. Los temas a tratar son los siguientes:
-        
-        div.row.justify-content-center.align-items-stretch.mb-3
-          div.col-lg-3.mb-4(data-aos="zoom-in-up")
-            div.bg-color-white.box-shadow.px-4.py-5.h-100
-              img.mx-auto.d-block.mb-4(src="@/assets/curso/intro/2.svg" alt="" style="width: 90px")
-              h5.text-center Tema 1
-              p.mb-0.text-center Conceptos y características de los recursos educativos digitales.
-        
-          div.col-lg-3.mb-4(data-aos="zoom-in-up")
-            div.bg-color-white.box-shadow.px-4.py-5.h-100
-              img.mx-auto.d-block.mb-4(src="@/assets/curso/intro/2.svg" alt="" style="width: 90px")
-              h5.text-center Tema 2
-              p.mb-0.text-center Accesibilidad y usabilidad en los recursos digitales.
-        
-          div.col-lg-3.mb-4(data-aos="zoom-in-up")
-            div.bg-color-white.box-shadow.px-4.py-5.h-100
-              img.mx-auto.d-block.mb-4(src="@/assets/curso/intro/2.svg" alt="" style="width: 90px")
-              h5.text-center Tema 3
-              p.mb-0.text-center Plataformas de diseño de contenidos digitales
-          div.col-lg-3.mb-4(data-aos="zoom-in-up")
-            div.bg-color-white.box-shadow.px-4.py-5.h-100
-              img.mx-auto.d-block.mb-4(src="@/assets/curso/intro/2.svg" alt="" style="width: 90px")
-              h5.text-center Tema 4
-              p.mb-0.text-center Herramientas para la gamificación y evaluación: Kahoot, Quizizz, Mentimeter
+    div.row.justify-content-center.align-items-stretch.mb-5
+      div.col-lg-4.mb-4(data-aos="zoom-in-up")
+        .tarjeta.bg-color-1.px-4.py-5.h-100
+          p.mb-0 La transformación digital en la educación, ha generado nuevas formas de enseñar y aprender. En este contexto, el uso de herramientas digitales para crear materiales educativos interactivos, se convierte en una competencia esencial para los profesionales de la educación. Esta unidad tiene como propósito, brindar al estudiante los conocimientos y habilidades necesarios para utilizar herramientas digitales en la creación de recursos educativos que sean dinámicos, atractivos y pedagógicamente eficaces.
+    
+      div.col-lg-4.mb-4(data-aos="zoom-in-up")
+        img.mx-auto.d-block.mb-4(
+          src="@/assets/curso/intro/1.png"
+          alt=""
+        )
+    
+      div.col-lg-4.mb-4(data-aos="zoom-in-up")
+        .tarjeta.bg-color-2.px-4.py-5.h-100
+          p.mb-3 A lo largo de la unidad, se aprenderá a seleccionar herramientas digitales adecuadas para cada necesidad formativa, considerando criterios pedagógicos, técnicos y comunicativos. Se explorarán plataformas de diseño de contenidos digitales como H5P, Genially, Articulate y Canva, ideales para producir materiales visuales e interactivos que apoyen distintos estilos de aprendizaje.
+          div.d-flex.justify-content-end
+            img.icono-right(
+              src="@/assets/curso/tema1/2.svg"
+              alt=""
+              style="width: 105px"
+            )
 
-        div.row.justify-content-center.align-items-stretch
-          div.col-lg-3.mb-4(data-aos="zoom-in-up")
-            div.bg-color-white.box-shadow.px-4.py-5.h-100
-              img.mx-auto.d-block.mb-4(src="@/assets/curso/intro/2.svg" alt="" style="width: 90px")
-              h5.text-center Tema 5
-              p.mb-0.text-center Estrategias para la integración de recursos digitales en entornos virtuales de aprendizaje (EVA)
-        
-          div.col-lg-3.mb-4(data-aos="zoom-in-up")
-            div.bg-color-white.box-shadow.px-4.py-5.h-100
-              img.mx-auto.d-block.mb-4(src="@/assets/curso/intro/2.svg" alt="" style="width: 90px")
-              h5.text-center Tema 6
-              p.mb-0.text-center Mejora y optimización de materiales digitales según retroalimentación y análisis de datos
+    .row.mb-5
+      .col-lg-8
+        p(data-aos="fade-left") También se abordará la creación de videos educativos y screencasts, utilizando herramientas como Camtasia, OBS Studio y Powtoon, que permiten desarrollar explicaciones visuales, demostraciones paso a paso y narrativas digitales efectivas. Se profundizará en el uso de herramientas de gamificación y evaluación como Kahoot, Quizizz y Mentimeter, que facilitan la retroalimentación inmediata, fomentan la motivación y mejoran el compromiso del estudiante.
+    
+        .bg-color-2.p-4(data-aos="fade-left").mb-4
+          p.mb-0 Además, se introducirá el diseño de experiencias de aprendizaje inmersivas, mediante el uso de tecnologías de Realidad Aumentada y Virtual, con el fin de crear entornos que favorezcan la exploración, la práctica y la comprensión profunda.
 
-    .bg-full-width.bg-color-2.mb-5
-      .px-4.px-md-5.py-4
-        .row.align-items-center
-          .col-lg-auto
-            img(src="@/assets/curso/intro/3.svg", style="max-width: 90px").mx-auto
-          .col-lg
-            p.mb-0 Se espera que participe activamente en las actividades propuestas, que incluyen, entre otros, lecturas, discusiones orientadas a promover un aprendizaje significativo. Aunque no se requieren conocimientos previos avanzados, contar con una comprensión básica sobre los recursos educativos, será de gran utilidad para asimilar con mayor profundidad, los conceptos que se desarrollarán a lo largo de la unidad.
+        p.mb-0 Se espera que participe activamente en las actividades propuestas, que incluyen, entre otros, lecturas, discusiones orientadas a promover un aprendizaje significativo. Aunque no se requieren conocimientos previos avanzados, contar con una comprensión básica sobre las herramientas tecnológicas, será de gran utilidad para asimilar con mayor profundidad los conceptos que se desarrollarán a lo largo de la unidad.
+    
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/intro/3.png", data-aos="zoom-in")
+    
 
 </template>
